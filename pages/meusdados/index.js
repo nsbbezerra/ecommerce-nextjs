cb98ffec369301715a17a75ffe52154d265cf3ad
Natanael Bezerra <nsbbezerra@hotmail.com>
2020-12-23 12:06:53 -0300

    finalize some pages

--- pages/meusdados/index.js
+++ pages/meusdados/index.js
@@ -33,10 +33,12 @@ import {
   AiOutlinePlus,
   AiOutlineKey,
   AiOutlineSave,
+  AiOutlineMenu,
 } from "react-icons/ai";
 import { FaTimes } from "react-icons/fa";
 import { FiMapPin } from "react-icons/fi";
-
+import Link from "next/link";
+import { BsBag } from "react-icons/bs";
 import configs from "../../configs/configs";
 
 export default function MeusDados() {
@@ -50,206 +52,252 @@ export default function MeusDados() {
       </ContainerNonFixed>
       <Fixed>
         <Container maxW="6xl" mt={10} mb={10}>
-          <Flex
-            bg={configs.template.chart.bgBanner}
-            p={3}
-            shadow="md"
-            borderRadius="md"
-            mb={5}
-            align="center"
+          <Grid
+            templateColumns={["1fr", "1fr", "1fr", "1fr", "200px 1fr"]}
+            gap={10}
           >
-            <Icon as={AiOutlineIdcard} fontSize="2xl" mr={10} />
-            <Heading size="md" color={configs.template.chart.colorBanner}>
-              Meus Dados
-            </Heading>
-          </Flex>
-
-          <Box borderWidth="1px" p={5} borderRadius="md" mt={5} shadow="md">
-            <Flex align="center">
-              <Icon as={AiOutlineIdcard} mr={5} />
-              <Text fontSize="md" fontWeight="700">
-                Dados
-              </Text>
-            </Flex>
-            <Divider mt={3} mb={3} />
-
-            <Grid templateColumns="1fr" gap="20px">
-              <Box>
-                <Text fontSize="sm">Nome</Text>
-                <Input
-                  type="number"
-                  value={""}
-                  _focus={{ borderColor: "yellow.400" }}
-                  isReadOnly
-                />
-              </Box>
-            </Grid>
-
-            <Grid
-              templateColumns={[
-                "100%",
-                "100%",
-                "1fr 1fr",
-                "1fr 1fr",
-                "1fr 1fr",
-              ]}
-              gap="20px"
-              mt={3}
+            <Box
+              borderRadius="md"
+              borderWidth="1px"
+              p={3}
+              h="min-content"
+              display={["none", "none", "none", "none", "block"]}
             >
-              <Box>
-                <Text fontSize="sm">Email</Text>
-                <Input
-                  type="number"
-                  value={""}
-                  _focus={{ borderColor: "yellow.400" }}
-                  isReadOnly
-                />
-              </Box>
-              <Box>
-                <Text fontSize="sm">Telefone</Text>
-                <Input
-                  type="number"
-                  value={""}
-                  _focus={{ borderColor: "yellow.400" }}
-                  isReadOnly
-                />
-              </Box>
-            </Grid>
+              <Flex fontWeight="700" align="center">
+                <Icon as={AiOutlineMenu} mr={5} />
+                Menu
+              </Flex>
 
-            <Grid
-              templateColumns={[
-                "100%",
-                "100%",
-                "1fr 1fr",
-                "1fr 1fr",
-                "1fr 1fr",
-              ]}
-              gap="20px"
-              mt={3}
-            >
-              <Box>
-                <Text fontSize="sm">CPF</Text>
-                <Input
-                  type="number"
-                  value={""}
-                  _focus={{ borderColor: "yellow.400" }}
-                  isReadOnly
-                />
-              </Box>
-              <Box>
-                <Text fontSize="sm" color="white" userSelect="none">
-                  CPF
-                </Text>
-                <Button
-                  leftIcon={<AiOutlineKey />}
-                  onClick={() => setAlert(true)}
-                >
-                  Alterar Senha de Acesso
-                </Button>
-              </Box>
-            </Grid>
-          </Box>
+              <Link href="/meusdados" passHref>
+                <a>
+                  <Button
+                    leftIcon={<AiOutlineIdcard />}
+                    variant="solid"
+                    isFullWidth
+                    mt={5}
+                  >
+                    Meus Dados
+                  </Button>
+                </a>
+              </Link>
 
-          {/** DADOS DO ENDEREÇO */}
-
-          <Box borderWidth="1px" p={5} borderRadius="md" mt={5} shadow="md">
-            <Flex align="center">
-              <Icon as={FiMapPin} mr={5} />
-              <Text fontSize="md" fontWeight="700">
-                Endereço
-              </Text>
-            </Flex>
-            <Divider mt={3} mb={3} />
-
-            <Box borderWidth="1px" p={3} borderRadius="md">
-              <Grid
-                templateColumns={[
-                  "100%",
-                  "100%",
-                  "1fr 100px",
-                  "1fr 100px",
-                  "1fr 100px",
-                ]}
-                gap="20px"
-              >
-                <Box>
-                  <Text fontSize="sm">Logradouro</Text>
-                  <Input
-                    type="number"
-                    value={""}
-                    _focus={{ borderColor: "yellow.400" }}
-                    isReadOnly
-                  />
-                </Box>
-                <Box>
-                  <Text fontSize="sm">Número</Text>
-                  <Input
-                    type="number"
-                    value={""}
-                    _focus={{ borderColor: "yellow.400" }}
-                    isReadOnly
-                  />
-                </Box>
-              </Grid>
-              <Text fontSize="sm" mt={3}>
-                Complemento
-              </Text>
-              <Input
-                type="number"
-                value={""}
-                _focus={{ borderColor: "yellow.400" }}
-                isReadOnly
-              />
-
-              <Grid
-                templateColumns={[
-                  "100%",
-                  "100%",
-                  "1fr 1fr 100px",
-                  "1fr 1fr 100px",
-                  "1fr 1fr 100px",
-                ]}
-                gap="20px"
-                mt={3}
-              >
-                <Box>
-                  <Text fontSize="sm">CEP</Text>
-                  <Input
-                    type="number"
-                    value={""}
-                    _focus={{ borderColor: "yellow.400" }}
-                    isReadOnly
-                  />
-                </Box>
-                <Box>
-                  <Text fontSize="sm">Cidade</Text>
-                  <Input
-                    type="number"
-                    value={""}
-                    _focus={{ borderColor: "yellow.400" }}
-                    isReadOnly
-                  />
-                </Box>
-                <Box>
-                  <Text fontSize="sm">Estado</Text>
-                  <Select placeholder="UF" focusBorderColor="yellow.400">
-                    <option value="option1">Option 1</option>
-                    <option value="option2">Option 2</option>
-                    <option value="option3">Option 3</option>
-                  </Select>
-                </Box>
-              </Grid>
+              <Link href="/minhascompras" passHref>
+                <a>
+                  <Button
+                    leftIcon={<BsBag />}
+                    variant="solid"
+                    mt={5}
+                    isFullWidth
+                  >
+                    Minhas Compras
+                  </Button>
+                </a>
+              </Link>
             </Box>
 
-            <Divider mt={5} mb={5} />
+            <Box>
+              <Flex
+                bg={configs.template.chart.bgBanner}
+                p={3}
+                shadow="md"
+                borderRadius="md"
+                mb={5}
+                align="center"
+              >
+                <Icon as={AiOutlineIdcard} fontSize="2xl" mr={10} />
+                <Heading size="md" color={configs.template.chart.colorBanner}>
+                  Meus Dados
+                </Heading>
+              </Flex>
 
-            <Button
-              leftIcon={<AiOutlinePlus />}
-              onClick={() => setModalAddress(true)}
-            >
-              Adicionar Endereço
-            </Button>
-          </Box>
+              <Box borderWidth="1px" p={5} borderRadius="md" mt={5} shadow="md">
+                <Flex align="center">
+                  <Icon as={AiOutlineIdcard} mr={5} />
+                  <Text fontSize="md" fontWeight="700">
+                    Dados
+                  </Text>
+                </Flex>
+                <Divider mt={3} mb={3} />
+
+                <Grid templateColumns="1fr" gap="20px">
+                  <Box>
+                    <Text fontSize="sm">Nome</Text>
+                    <Input
+                      type="number"
+                      value={""}
+                      _focus={{ borderColor: "yellow.400" }}
+                      isReadOnly
+                    />
+                  </Box>
+                </Grid>
+
+                <Grid
+                  templateColumns={[
+                    "100%",
+                    "100%",
+                    "1fr 1fr",
+                    "1fr 1fr",
+                    "1fr 1fr",
+                  ]}
+                  gap="20px"
+                  mt={3}
+                >
+                  <Box>
+                    <Text fontSize="sm">Email</Text>
+                    <Input
+                      type="number"
+                      value={""}
+                      _focus={{ borderColor: "yellow.400" }}
+                      isReadOnly
+                    />
+                  </Box>
+                  <Box>
+                    <Text fontSize="sm">Telefone</Text>
+                    <Input
+                      type="number"
+                      value={""}
+                      _focus={{ borderColor: "yellow.400" }}
+                      isReadOnly
+                    />
+                  </Box>
+                </Grid>
+
+                <Grid
+                  templateColumns={[
+                    "100%",
+                    "100%",
+                    "1fr 1fr",
+                    "1fr 1fr",
+                    "1fr 1fr",
+                  ]}
+                  gap="20px"
+                  mt={3}
+                >
+                  <Box>
+                    <Text fontSize="sm">CPF</Text>
+                    <Input
+                      type="number"
+                      value={""}
+                      _focus={{ borderColor: "yellow.400" }}
+                      isReadOnly
+                    />
+                  </Box>
+                  <Box>
+                    <Text fontSize="sm" color="white" userSelect="none">
+                      CPF
+                    </Text>
+                    <Button
+                      leftIcon={<AiOutlineKey />}
+                      onClick={() => setAlert(true)}
+                    >
+                      Alterar Senha de Acesso
+                    </Button>
+                  </Box>
+                </Grid>
+              </Box>
+
+              {/** DADOS DO ENDEREÇO */}
+
+              <Box borderWidth="1px" p={5} borderRadius="md" mt={5} shadow="md">
+                <Flex align="center">
+                  <Icon as={FiMapPin} mr={5} />
+                  <Text fontSize="md" fontWeight="700">
+                    Endereço
+                  </Text>
+                </Flex>
+                <Divider mt={3} mb={3} />
+
+                <Box borderWidth="1px" p={3} borderRadius="md">
+                  <Grid
+                    templateColumns={[
+                      "100%",
+                      "100%",
+                      "1fr 100px",
+                      "1fr 100px",
+                      "1fr 100px",
+                    ]}
+                    gap="20px"
+                  >
+                    <Box>
+                      <Text fontSize="sm">Logradouro</Text>
+                      <Input
+                        type="number"
+                        value={""}
+                        _focus={{ borderColor: "yellow.400" }}
+                        isReadOnly
+                      />
+                    </Box>
+                    <Box>
+                      <Text fontSize="sm">Número</Text>
+                      <Input
+                        type="number"
+                        value={""}
+                        _focus={{ borderColor: "yellow.400" }}
+                        isReadOnly
+                      />
+                    </Box>
+                  </Grid>
+                  <Text fontSize="sm" mt={3}>
+                    Complemento
+                  </Text>
+                  <Input
+                    type="number"
+                    value={""}
+                    _focus={{ borderColor: "yellow.400" }}
+                    isReadOnly
+                  />
+
+                  <Grid
+                    templateColumns={[
+                      "100%",
+                      "100%",
+                      "1fr 1fr 100px",
+                      "1fr 1fr 100px",
+                      "1fr 1fr 100px",
+                    ]}
+                    gap="20px"
+                    mt={3}
+                  >
+                    <Box>
+                      <Text fontSize="sm">CEP</Text>
+                      <Input
+                        type="number"
+                        value={""}
+                        _focus={{ borderColor: "yellow.400" }}
+                        isReadOnly
+                      />
+                    </Box>
+                    <Box>
+                      <Text fontSize="sm">Cidade</Text>
+                      <Input
+                        type="number"
+                        value={""}
+                        _focus={{ borderColor: "yellow.400" }}
+                        isReadOnly
+                      />
+                    </Box>
+                    <Box>
+                      <Text fontSize="sm">Estado</Text>
+                      <Select placeholder="UF" focusBorderColor="yellow.400">
+                        <option value="option1">Option 1</option>
+                        <option value="option2">Option 2</option>
+                        <option value="option3">Option 3</option>
+                      </Select>
+                    </Box>
+                  </Grid>
+                </Box>
+
+                <Divider mt={5} mb={5} />
+
+                <Button
+                  leftIcon={<AiOutlinePlus />}
+                  onClick={() => setModalAddress(true)}
+                >
+                  Adicionar Endereço
+                </Button>
+              </Box>
+            </Box>
+          </Grid>
         </Container>
       </Fixed>
       <ContainerNonFixed>
--- pages/minhascompras/index.js
+++ pages/minhascompras/index.js
@@ -10,11 +10,14 @@ import {
   Badge,
   Button,
   Text,
+  Grid,
 } from "@chakra-ui/react";
-import { AiOutlineShoppingCart } from "react-icons/ai";
+import { AiOutlineMenu, AiOutlineIdcard } from "react-icons/ai";
 import configs from "../../configs/configs";
 import { BiDetail } from "react-icons/bi";
 import { GoPackage } from "react-icons/go";
+import { BsBag } from "react-icons/bs";
+import Link from "next/link";
 
 export default function MinhasCompras() {
   return (
@@ -24,67 +27,215 @@ export default function MinhasCompras() {
       </ContainerNonFixed>
       <Fixed>
         <Container maxW="6xl" mt={10} mb={10}>
-          <Flex
-            bg={configs.template.chart.bgBanner}
-            p={3}
-            shadow="md"
-            borderRadius="md"
-            mb={5}
-            align="center"
+          <Grid
+            templateColumns={["1fr", "1fr", "1fr", "1fr", "200px 1fr"]}
+            gap={10}
           >
-            <Icon as={AiOutlineShoppingCart} fontSize="2xl" mr={10} />
-            <Heading size="md" color={configs.template.chart.colorBanner}>
-              Minhas Compras
-            </Heading>
-          </Flex>
-
-          <Flex
-            wrap="wrap"
-            shadow="md"
-            borderRadius="md"
-            borderWidth="1px"
-            p={5}
-            align="center"
-            justify="space-between"
-          >
-            <Flex
-              h={"60px"}
-              w="60px"
-              bg="gray.100"
-              align="center"
-              justify="center"
-              color="gray.700"
-              borderRadius="50%"
+            <Box
+              borderRadius="md"
+              borderWidth="1px"
+              p={3}
+              h="min-content"
+              display={["none", "none", "none", "none", "block"]}
             >
-              <Icon as={GoPackage} fontSize="4xl" />
-            </Flex>
-            <Box>
-              <Heading size="md">Número:</Heading>
-              <Text>1293813</Text>
+              <Flex fontWeight="700" align="center">
+                <Icon as={AiOutlineMenu} mr={5} />
+                Menu
+              </Flex>
+
+              <Link href="/meusdados" passHref>
+                <a>
+                  <Button
+                    leftIcon={<AiOutlineIdcard />}
+                    variant="solid"
+                    isFullWidth
+                    mt={5}
+                  >
+                    Meus Dados
+                  </Button>
+                </a>
+              </Link>
+
+              <Link href="/minhascompras" passHref>
+                <a>
+                  <Button
+                    leftIcon={<BsBag />}
+                    variant="solid"
+                    mt={5}
+                    isFullWidth
+                  >
+                    Minhas Compras
+                  </Button>
+                </a>
+              </Link>
             </Box>
+
             <Box>
-              <Heading size="md">Data:</Heading>
-              <Text>21/20/1929</Text>
+              <Flex
+                bg={configs.template.chart.bgBanner}
+                p={3}
+                shadow="md"
+                borderRadius="md"
+                mb={5}
+                align="center"
+              >
+                <Icon as={BsBag} fontSize="2xl" mr={10} />
+                <Heading size="md" color={configs.template.chart.colorBanner}>
+                  Minhas Compras
+                </Heading>
+              </Flex>
+
+              <Flex
+                wrap="wrap"
+                shadow="md"
+                borderRadius="md"
+                borderWidth="1px"
+                p={5}
+                align="center"
+                justify="space-between"
+                mb={5}
+              >
+                <Flex
+                  h={"60px"}
+                  w="60px"
+                  bg="gray.100"
+                  align="center"
+                  justify="center"
+                  color="gray.700"
+                  borderRadius="50%"
+                  mr={2}
+                >
+                  <Icon as={GoPackage} fontSize="4xl" />
+                </Flex>
+                <Box mr={2}>
+                  <Heading size="md">Número:</Heading>
+                  <Text>1293813</Text>
+                </Box>
+                <Box mr={2}>
+                  <Heading size="md">Data:</Heading>
+                  <Text>21/20/1929</Text>
+                </Box>
+                <Box mr={2}>
+                  <Heading size="md">Status:</Heading>
+                  <Badge fontSize="lg" variant="solid" colorScheme="green">
+                    Em Preparação
+                  </Badge>
+                </Box>
+                <Box mr={2}>
+                  <Heading size="md">Valor:</Heading>
+                  <Text>R$ 250,00</Text>
+                </Box>
+                <Box mr={2}>
+                  <Heading size="md" color="white" userSelect="none">
+                    Data:
+                  </Heading>
+                  <Button w="200px" leftIcon={<BiDetail />}>
+                    Detalhes
+                  </Button>
+                </Box>
+              </Flex>
+
+              <Flex
+                wrap="wrap"
+                shadow="md"
+                borderRadius="md"
+                borderWidth="1px"
+                p={5}
+                align="center"
+                justify="space-between"
+                mb={5}
+              >
+                <Flex
+                  h={"60px"}
+                  w="60px"
+                  bg="gray.100"
+                  align="center"
+                  justify="center"
+                  color="gray.700"
+                  borderRadius="50%"
+                  mr={2}
+                >
+                  <Icon as={GoPackage} fontSize="4xl" />
+                </Flex>
+                <Box mr={2}>
+                  <Heading size="md">Número:</Heading>
+                  <Text>1293813</Text>
+                </Box>
+                <Box mr={2}>
+                  <Heading size="md">Data:</Heading>
+                  <Text>21/20/1929</Text>
+                </Box>
+                <Box mr={2}>
+                  <Heading size="md">Status:</Heading>
+                  <Badge fontSize="lg" variant="solid" colorScheme="green">
+                    Em Preparação
+                  </Badge>
+                </Box>
+                <Box mr={2}>
+                  <Heading size="md">Valor:</Heading>
+                  <Text>R$ 250,00</Text>
+                </Box>
+                <Box mr={2}>
+                  <Heading size="md" color="white" userSelect="none">
+                    Data:
+                  </Heading>
+                  <Button w="200px" leftIcon={<BiDetail />}>
+                    Detalhes
+                  </Button>
+                </Box>
+              </Flex>
+
+              <Flex
+                wrap="wrap"
+                shadow="md"
+                borderRadius="md"
+                borderWidth="1px"
+                p={5}
+                align="center"
+                justify="space-between"
+                mb={5}
+              >
+                <Flex
+                  h={"60px"}
+                  w="60px"
+                  bg="gray.100"
+                  align="center"
+                  justify="center"
+                  color="gray.700"
+                  borderRadius="50%"
+                  mr={2}
+                >
+                  <Icon as={GoPackage} fontSize="4xl" />
+                </Flex>
+                <Box mr={2}>
+                  <Heading size="md">Número:</Heading>
+                  <Text>1293813</Text>
+                </Box>
+                <Box mr={2}>
+                  <Heading size="md">Data:</Heading>
+                  <Text>21/20/1929</Text>
+                </Box>
+                <Box mr={2}>
+                  <Heading size="md">Status:</Heading>
+                  <Badge fontSize="lg" variant="solid" colorScheme="green">
+                    Em Preparação
+                  </Badge>
+                </Box>
+                <Box mr={2}>
+                  <Heading size="md">Valor:</Heading>
+                  <Text>R$ 250,00</Text>
+                </Box>
+                <Box mr={2}>
+                  <Heading size="md" color="white" userSelect="none">
+                    Data:
+                  </Heading>
+                  <Button w="200px" leftIcon={<BiDetail />}>
+                    Detalhes
+                  </Button>
+                </Box>
+              </Flex>
             </Box>
-            <Box>
-              <Heading size="md">Status:</Heading>
-              <Badge fontSize="lg" variant="solid" colorScheme="green">
-                Em Preparação
-              </Badge>
-            </Box>
-            <Box>
-              <Heading size="md">Valor:</Heading>
-              <Text>R$ 250,00</Text>
-            </Box>
-            <Box>
-              <Heading size="md" color="white" userSelect="none">
-                Data:
-              </Heading>
-              <Button w="200px" leftIcon={<BiDetail />}>
-                Detalhes
-              </Button>
-            </Box>
-          </Flex>
+          </Grid>
         </Container>
       </Fixed>
       <ContainerNonFixed>
